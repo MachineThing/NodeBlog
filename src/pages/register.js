8021bf131623264c32b17a399d3bd1757fbb4965
Mason Fisher <sackzombie10@gmail.com>
2022-07-25 09:47:25 -0400

    Making hashes

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 const { URLSearchParams } = require('url');
 
 module.exports = (req, res) => {
@@ -24,7 +25,15 @@ module.exports = (req, res) => {
                 vaild_username = /^[A-Za-z0-9-_]{3,20}$/.test(outParams.get('username'));
                 // Check if email matches (credit to: https://github.com/manishsaraan/email-validator/blob/master/index.js)
                 valid_email = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/.test(outParams.get('email'));
-                resolve(`1: ${password_matches} 2: ${vaild_username} 3: ${valid_email}`);
+                if (!password_matches || !vaild_username || !valid_email) {
+                    resolve('Error');
+                } else {
+                    let salt = crypto.randomBytes(16);
+                    let user_password = `${salt}${outParams.get('password1')}`
+                    let hash = crypto.createHash('sha3-384').update(user_password).digest('hex');
+                    let salted_hash = `${salt.toString('hex')}${hash}`
+                    resolve(salted_hash);
+                }
             });
         } else {
             resolve(fs.readFileSync('./src/html/register.html'));
